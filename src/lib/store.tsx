550f78812e12679d0f44bb687d10a4407433bb1c
dura12 <duranu104@gmail.com>
2024-08-28 10:05:45 +0300

    updated the navbar and the card

--- src/lib/store.tsx
+++ src/lib/store.tsx
@@ -1,20 +1,16 @@
 // src/app/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
-import { JobsApi } from './services/opportunitiesService'; // Ensure correct import path
+import { JobsApi } from './services/opportunitiesService'; 
 import { BookmarkApi } from './services/bookmarkService';
 export const store = configureStore({
   reducer: {
     [JobsApi.reducerPath]: JobsApi.reducer, 
-    [BookmarkApi.reducerPath]: BookmarkApi.reducer, // Add the API slice reducer
+    [BookmarkApi.reducerPath]: BookmarkApi.reducer, 
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(JobsApi.middleware),
-
-   // Add RTK Query middleware
 });
-
-// Setup listeners for refetching and reconnecting
 setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
